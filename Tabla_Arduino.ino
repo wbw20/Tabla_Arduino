f75f8859046be15534fb24ea6ed2a3d6131c4ec5
William B. Wettersten <wbw20@case.edu>
2014-04-24 22:25:18 -0400

    use variance

--- Tabla_Arduino.ino
+++ Tabla_Arduino.ino
@@ -1,554 +1,317 @@
-// X0 = 41
-// X1 = 39
-// X2 = 37
-// X3 = 35
-// X4 = 33
-// X5 = 31
-// X6 = 29
-// X7 = 27
-// X8 = 25
-// X9 = 23
-// X10 = 22
-// X11 = 21
-// X12 = 20
-// X13 = 19
-// X14 = 18
-// X15 = 17
-// X16 = 16
-// X17 = 15
-// X18 = 0 -- But this pin is needed for USB
-// X19 = 1 -- But this pin is needed for USB
-// X20 = 2
-// X21 = 3
-// X22 = 4
-// X23 = 5
-// X24 = 6
-// X25 = 7
-// X26 = 8
-// X27 = 9
-// X28 = 10
-// X29 = AREF
-// X30 = SDA1
+/*      Generated 24/04/2014 22:02:12      */
 
-// Y0 = CANTX
-// Y1 = DEAD PIN -- oops!
-// Y2 = 53
-// Y3 = 51
-// Y4 = 49
-// Y5 = 47
-// Y6 = 45
-// Y7 = 43
-// Y8 = 24
-// Y9 = 26
-// Y10 = 28
-// Y11 = 30
-// Y12 = 32
-// Y13 = 34
-// Y14 = 36
-// Y15 = 38
-// Y16 = 40
-// Y17 = 42
-// Y18 = 44
-// Y19 = 46
-// Y20 = 48
-// Y21 = 50
-// Y22 = 52
-// Y23 = A1
-// Y24 = A2
-// Y25 = A4
-// Y26 = A5
-// Y27 = A6
-// Y28 = A8
-// Y29 = A10
-// Y30 = A11
 
-int x[27] = {41, 39, 37, 35, 33, 31, 29, 27, 25, 23, 22, 21, 20, 19, 18, 17, 16, 15, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int y[21] = {53, 51, 49, 47, 45, 43, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52};
+float x_average = 0;
+float y_average = 0;
 
-bool mutex = false;
+float x_variance = 0;
+float y_variance = 0;
 
-/*
- *  Vertical lines on the pad
- */
+int x[27] = { 41, 39, 37, 35, 33, 31, 29, 27, 25, 23, 22, 21, 20, 19, 18, 17, 16, 15, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int y[21] = { 53, 51, 49, 47, 45, 43, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52 };
+
+void update_x(int value) {
+Serial.println(x_average == 0 ? 0 : (x_average - value));
+  x_variance += x_average == 0 ? 0 : (x_average - value);
+  x_average = value;
+}
+
+void update_y(int value) {
+  y_variance += y_average == 0 ? 0 : (y_average - value);
+  y_average = value;
+}
+
 void x_0() {
-  if (mutex) {
-    return;
-  }
-  
-  mutex = true;
-
-  Serial.write("[0, 15]\n");
+  update_x(0);
 }
 
 void x_1() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("[1, 15]\n");
+  update_x(1);
 }
 
 void x_2() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("[2, 15]\n");
+  update_x(2);
 }
 
 void x_3() {
-  if (mutex) {
-    return;
-  }
-  
-  mutex = true;
-
-  Serial.write("[3, 15]\n");
+  update_x(3);
 }
 
 void x_4() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("[4, 15]\n");
+  update_x(4);
 }
 
 void x_5() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("[5, 15]\n");
+  update_x(5);
 }
 
 void x_6() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("[6, 15]\n");
+  update_x(6);
 }
 
 void x_7() {
-  if (mutex) {
-    return;
-  }
-  
-  mutex = true;
-
-  Serial.write("[7, 15]\n");
+  update_x(7);
 }
 
 void x_8() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("[8, 15]\n");
+  update_x(8);
 }
 
 void x_9() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("[9, 15]\n");
+  update_x(9);
 }
 
 void x_10() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("[10, 15]\n");
+  update_x(10);
 }
 
 void x_11() {
-  if (mutex) {
-    return;
-  }
-  
-  mutex = true;
- 
-  Serial.write("[11, 15]\n");
+  update_x(11);
 }
 
 void x_12() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 12\n");
+  update_x(12);
 }
 
 void x_13() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 13\n");
+  update_x(13);
 }
 
 void x_14() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 14\n");
+  update_x(14);
 }
 
 void x_15() {
-  if (mutex) {
-    return;
-  }
-  
-  mutex = true;
-
-  Serial.write("X --> 15\n");
+  update_x(15);
 }
 
 void x_16() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 16\n");
+  update_x(16);
 }
 
 void x_17() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 17\n");
+  update_x(17);
 }
 
 void x_18() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 18\n");
+  update_x(18);
 }
 
 void x_19() {
-  if (mutex) {
-    return;
-  }
-  
-  mutex = true;
-
-  Serial.write("X --> 19\n");
+  update_x(19);
 }
 
 void x_20() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 20\n");
+  update_x(20);
 }
 
 void x_21() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 21\n");
+  update_x(21);
 }
 
 void x_22() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 22\n");
+  update_x(22);
 }
 
 void x_23() {
-  if (mutex) {
-    return;
-  }
-  
-  mutex = true;
-
-  Serial.write("X --> 23\n");
+  update_x(23);
 }
 
 void x_24() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 24\n");
+  update_x(24);
 }
 
 void x_25() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 25\n");
+  update_x(25);
 }
 
 void x_26() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 26\n");
+  update_x(26);
 }
 
-void x_27() {
-  if (mutex) {
-    return;
-  }
-  
-  mutex = true;
-
-  Serial.write("X --> 27\n");
-}
-
-void x_28() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 28\n");
-}
-
-void x_29() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 29\n");
-}
-
-void x_30() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-  Serial.write("X --> 30\n");
-}
-
- void x_31() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-   Serial.write("X --> 31\n");
- }
- 
- void test() {
-  if (mutex) {
-    return;
-  }
-
-  mutex = true;
-
-   Serial.println("hit");
- }
-
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  // Columns
-  // for (int i = 0; i < (sizeof(x)/sizeof(int)); i+=8) {
-  //   delay(250);
-    
-  //   Serial.println(x[i]);
-    
-  //   delay(250);
-    
-  //   pinMode(x[i], INPUT_PULLUP);
-  //   digitalWrite(x[i], true);
-  //   attachInterrupt(x[i], test, CHANGE);
-  // }
 
-    pinMode(x[0], INPUT_PULLUP);
-    digitalWrite(x[0], true);
-    attachInterrupt(x[0], x_0, FALLING);
+  pinMode(x[0], INPUT_PULLUP);
+  digitalWrite(x[0], true);
+  attachInterrupt(x[0], x_0, FALLING);
 
-    pinMode(x[1], INPUT_PULLUP);
-    digitalWrite(x[1], true);
-    attachInterrupt(x[1], x_1, FALLING);
+  pinMode(x[1], INPUT_PULLUP);
+  digitalWrite(x[1], true);
+  attachInterrupt(x[1], x_1, FALLING);
 
-    pinMode(x[2], INPUT_PULLUP);
-    digitalWrite(x[2], true);
-    attachInterrupt(x[2], x_2, FALLING);
+  pinMode(x[2], INPUT_PULLUP);
+  digitalWrite(x[2], true);
+  attachInterrupt(x[2], x_2, FALLING);
 
-    pinMode(x[3], INPUT);
-    digitalWrite(x[3], true);
-    attachInterrupt(x[3], x_3, FALLING);
+  pinMode(x[3], INPUT_PULLUP);
+  digitalWrite(x[3], true);
+  attachInterrupt(x[3], x_3, FALLING);
 
-    pinMode(x[4], INPUT_PULLUP);
-    digitalWrite(x[4], true);
-    attachInterrupt(x[4], x_4, FALLING);
+  pinMode(x[4], INPUT_PULLUP);
+  digitalWrite(x[4], true);
+  attachInterrupt(x[4], x_4, FALLING);
 
-    pinMode(x[5], INPUT_PULLUP);
-    digitalWrite(x[5], true);
-    attachInterrupt(x[5], x_5, FALLING);
+  pinMode(x[5], INPUT_PULLUP);
+  digitalWrite(x[5], true);
+  attachInterrupt(x[5], x_5, FALLING);
 
-    pinMode(x[6], INPUT_PULLUP);
-    digitalWrite(x[6], true);
-    attachInterrupt(x[6], x_6, FALLING);
+  pinMode(x[6], INPUT_PULLUP);
+  digitalWrite(x[6], true);
+  attachInterrupt(x[6], x_6, FALLING);
 
-    pinMode(x[7], INPUT_PULLUP);
-    digitalWrite(x[7], true);
-    attachInterrupt(x[7], x_7, FALLING);
+  pinMode(x[7], INPUT_PULLUP);
+  digitalWrite(x[7], true);
+  attachInterrupt(x[7], x_7, FALLING);
 
-    pinMode(x[8], INPUT_PULLUP);
-    digitalWrite(x[8], true);
-    attachInterrupt(x[8], x_8, FALLING);
+  pinMode(x[8], INPUT_PULLUP);
+  digitalWrite(x[8], true);
+  attachInterrupt(x[8], x_8, FALLING);
 
-    pinMode(x[9], INPUT_PULLUP);
-    digitalWrite(x[9], true);
-    attachInterrupt(x[9], x_9, FALLING);
+  pinMode(x[9], INPUT_PULLUP);
+  digitalWrite(x[9], true);
+  attachInterrupt(x[9], x_9, FALLING);
 
-    pinMode(x[10], INPUT_PULLUP);
-    digitalWrite(x[10], true);
-    attachInterrupt(x[10], x_10, FALLING);
-    
-    pinMode(x[11], INPUT_PULLUP);
-    digitalWrite(x[11], true);
-    attachInterrupt(x[11], x_11, FALLING);
+  pinMode(x[10], INPUT_PULLUP);
+  digitalWrite(x[10], true);
+  attachInterrupt(x[10], x_10, FALLING);
 
-    pinMode(x[12], INPUT_PULLUP);
-    digitalWrite(x[12], true);
-    attachInterrupt(x[12], x_12, FALLING);
+  pinMode(x[11], INPUT_PULLUP);
+  digitalWrite(x[11], true);
+  attachInterrupt(x[11], x_11, FALLING);
 
-    pinMode(x[13], INPUT_PULLUP);
-    digitalWrite(x[13], true);
-    attachInterrupt(x[13], x_13, FALLING);
+  pinMode(x[12], INPUT_PULLUP);
+  digitalWrite(x[12], true);
+  attachInterrupt(x[12], x_12, FALLING);
 
-    pinMode(x[14], INPUT_PULLUP);
-    digitalWrite(x[14], true);
-    attachInterrupt(x[14], x_14, FALLING);
+  pinMode(x[13], INPUT_PULLUP);
+  digitalWrite(x[13], true);
+  attachInterrupt(x[13], x_13, FALLING);
 
-    pinMode(x[15], INPUT_PULLUP);
-    digitalWrite(x[15], true);
-    attachInterrupt(x[15], x_15, FALLING);
+  pinMode(x[14], INPUT_PULLUP);
+  digitalWrite(x[14], true);
+  attachInterrupt(x[14], x_14, FALLING);
 
-    pinMode(x[16], INPUT_PULLUP);
-    digitalWrite(x[16], true);
-    attachInterrupt(x[16], x_16, FALLING);
+  pinMode(x[15], INPUT_PULLUP);
+  digitalWrite(x[15], true);
+  attachInterrupt(x[15], x_15, FALLING);
 
-    pinMode(x[17], INPUT_PULLUP);
-    digitalWrite(x[17], true);
-    attachInterrupt(x[17], x_17, FALLING);
+  pinMode(x[16], INPUT_PULLUP);
+  digitalWrite(x[16], true);
+  attachInterrupt(x[16], x_16, FALLING);
 
-    pinMode(x[18], INPUT_PULLUP);
-    digitalWrite(x[18], true);
-    attachInterrupt(x[18], x_18, FALLING);
+  pinMode(x[17], INPUT_PULLUP);
+  digitalWrite(x[17], true);
+  attachInterrupt(x[17], x_17, FALLING);
 
-    pinMode(x[19], INPUT_PULLUP);
-    digitalWrite(x[19], true);
-    attachInterrupt(x[19], x_19, FALLING);
+  pinMode(x[18], INPUT_PULLUP);
+  digitalWrite(x[18], true);
+  attachInterrupt(x[18], x_18, FALLING);
 
-    pinMode(x[20], INPUT_PULLUP);
-    digitalWrite(x[20], true);
-    attachInterrupt(x[20], x_20, FALLING);
+  pinMode(x[19], INPUT_PULLUP);
+  digitalWrite(x[19], true);
+  attachInterrupt(x[19], x_19, FALLING);
 
-    pinMode(x[21], INPUT_PULLUP);
-    digitalWrite(x[21], true);
-    attachInterrupt(x[21], x_21, FALLING);
+  pinMode(x[20], INPUT_PULLUP);
+  digitalWrite(x[20], true);
+  attachInterrupt(x[20], x_20, FALLING);
 
-    pinMode(x[22], INPUT_PULLUP);
-    digitalWrite(x[22], true);
-    attachInterrupt(x[22], x_22, FALLING);
+  pinMode(x[21], INPUT_PULLUP);
+  digitalWrite(x[21], true);
+  attachInterrupt(x[21], x_21, FALLING);
 
-    pinMode(x[23], INPUT_PULLUP);
-    digitalWrite(x[23], true);
-    attachInterrupt(x[23], x_23, FALLING);
+  pinMode(x[22], INPUT_PULLUP);
+  digitalWrite(x[22], true);
+  attachInterrupt(x[22], x_22, FALLING);
 
-    pinMode(x[24], INPUT_PULLUP);
-    digitalWrite(x[24], true);
-    attachInterrupt(x[24], x_24, FALLING);
+  pinMode(x[23], INPUT_PULLUP);
+  digitalWrite(x[23], true);
+  attachInterrupt(x[23], x_23, FALLING);
 
-    pinMode(x[25], INPUT_PULLUP);
-    digitalWrite(x[25], true);
-    attachInterrupt(x[25], x_25, FALLING);
+  pinMode(x[24], INPUT_PULLUP);
+  digitalWrite(x[24], true);
+  attachInterrupt(x[24], x_24, FALLING);
 
-    pinMode(x[26], INPUT_PULLUP);
-    digitalWrite(x[26], true);
-    attachInterrupt(x[26], x_26, FALLING);
+  pinMode(x[25], INPUT_PULLUP);
+  digitalWrite(x[25], true);
+  attachInterrupt(x[25], x_25, FALLING);
 
-  Serial.println("setting up y");
+  pinMode(x[26], INPUT_PULLUP);
+  digitalWrite(x[26], true);
+  attachInterrupt(x[26], x_26, FALLING);
 
-  // Rows
-  for (int j = 0; j <= (sizeof(y)/sizeof(int)); j++) {
-    delay(250);
-    Serial.println(y[j]);
-    
-    delay(250);
-    
-    pinMode(y[j], OUTPUT);
-    digitalWrite(y[j], false);
-  }
-  
-  for (int j = 0; j <= (sizeof(y)/sizeof(int)); j++) {
-    
-  }
+  pinMode(y[0], OUTPUT);
+  digitalWrite(y[0], false);
+
+  pinMode(y[1], OUTPUT);
+  digitalWrite(y[1], false);
+
+  pinMode(y[2], OUTPUT);
+  digitalWrite(y[2], false);
+
+  pinMode(y[3], OUTPUT);
+  digitalWrite(y[3], false);
+
+  pinMode(y[4], OUTPUT);
+  digitalWrite(y[4], false);
+
+  pinMode(y[5], OUTPUT);
+  digitalWrite(y[5], false);
+
+  pinMode(y[6], OUTPUT);
+  digitalWrite(y[6], false);
+
+  pinMode(y[7], OUTPUT);
+  digitalWrite(y[7], false);
+
+  pinMode(y[8], OUTPUT);
+  digitalWrite(y[8], false);
+
+  pinMode(y[9], OUTPUT);
+  digitalWrite(y[9], false);
+
+  pinMode(y[10], OUTPUT);
+  digitalWrite(y[10], false);
+
+  pinMode(y[11], OUTPUT);
+  digitalWrite(y[11], false);
+
+  pinMode(y[12], OUTPUT);
+  digitalWrite(y[12], false);
+
+  pinMode(y[13], OUTPUT);
+  digitalWrite(y[13], false);
+
+  pinMode(y[14], OUTPUT);
+  digitalWrite(y[14], false);
+
+  pinMode(y[15], OUTPUT);
+  digitalWrite(y[15], false);
+
+  pinMode(y[16], OUTPUT);
+  digitalWrite(y[16], false);
+
+  pinMode(y[17], OUTPUT);
+  digitalWrite(y[17], false);
+
+  pinMode(y[18], OUTPUT);
+  digitalWrite(y[18], false);
+
+  pinMode(y[19], OUTPUT);
+  digitalWrite(y[19], false);
+
+  pinMode(y[20], OUTPUT);
+  digitalWrite(y[20], false);
+
 }
 
 void loop() {
-  delay(300);
-  
-  mutex = false;
+  delay(250);
+  if (x_variance > 0) {
+    Serial.println(x_average);
+    x_variance = 0;
+  }
+
+  x_average = 0;
 }
-
-
-
-
 
--- gen.py
+++ gen.py
@@ -8,6 +8,7 @@ y = [53, 51, 49, 47, 45, 43, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48,
 
 file.write("/*      Generated %s %s      */\n\n\n"%(time.strftime("%d/%m/%Y"), time.strftime("%H:%M:%S")))
 file.write("float x_average = 0;\nfloat y_average = 0;\n\n")
+file.write("float x_variance = 0;\nfloat y_variance = 0;\n\n")
 
 file.write("int x[%d] = { "%len(x))
 for value in itertools.islice(x, 0, len(x) - 1):
@@ -20,8 +21,8 @@ for value in itertools.islice(y, 0, len(y) - 1):
 file.write("%d };\n\n"%y[-1]) # y[-1] is the last element
 
 
-file.write("void update_x(int value) {\n  x_average = (x_average + value)/2;\n}\n\n")
-file.write("void update_y(int value) {\n  y_average = (y_average + value)/2;\n}\n\n")
+file.write("void update_x(int value) {\n  x_variance += x_average == 0 ? 0 : abs(x_average - value);\n  x_average = value;\n}\n\n")
+file.write("void update_y(int value) {\n  y_variance += y_average == 0 ? 0 : abs(y_average - value);\n  y_average = value;\n}\n\n")
 
 for value in xrange(len(x)):
   file.write("void x_%d() {\n  update_x(%d);\n}\n\n"%(value, value))
@@ -37,7 +38,7 @@ for value in xrange(len(y)):
   file.write("  pinMode(y[%d], OUTPUT);\n"%value)
   file.write("  digitalWrite(y[%d], false);\n\n"%value)
 
-file.write("}\n\nvoid loop() {\n  delay(250);\n  Serial.println(x_average);\n  x_average = 0;\n}\n")
+file.write("}\n\nvoid loop() {\n  delay(250);\n  if (x_variance > 0) {\n    Serial.println(x_average);\n    x_variance = 0;\n  }\n\n  x_average = 0;\n}\n")
 
 file.close()
 
